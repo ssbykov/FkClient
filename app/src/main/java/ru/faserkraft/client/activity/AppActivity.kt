bd74e5497024cd57e25762d340aef644322e9ee9
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-19 20:21:09 +0300

    сделана миграция на фрагменты

---
+++ app/src/main/java/ru/faserkraft/client/activity/AppActivity.kt
@@ -0,0 +1,27 @@
+package ru.faserkraft.client.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import ru.faserkraft.client.R
+import ru.faserkraft.client.databinding.AppActivityBinding
+
+
+class AppActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = AppActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.navigate(R.id.scannerFragment)
+    }
+}
