83eeee70df81ba551835d0169ec9d5153b7fd9b4
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-20 15:17:15 +0300

    добавил зависимость mapstruct

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,6 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.mapstruct)
+    annotationProcessor(libs.mapstruct.processor)
 }
