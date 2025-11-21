22b4c3fc99df22bfae4b5578a13acfe52aacd516
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-21 20:12:49 +0300

    незначительные изменения

--- app/src/main/java/ru/faserkraft/client/error/AppError.kt
+++ app/src/main/java/ru/faserkraft/client/error/AppError.kt
@@ -2,11 +2,10 @@ package ru.faserkraft.client.error
 
 import java.io.Serializable
 
-sealed class AppError(open var code: String) : RuntimeException(), java.io.Serializable
+sealed class AppError(open var code: String) : RuntimeException(), Serializable
 
 class ApiError(val status: Int, override var code: String) : AppError(code), Serializable
 
 object NetworkError : AppError("error_network")
-object AwsError : AppError("aws_error")
-object UnknownError: AppError("error_unknown")
-object DaoError: AppError("dao_error")
+object UnknownError : AppError("error_unknown")
+object DaoError : AppError("dao_error")
