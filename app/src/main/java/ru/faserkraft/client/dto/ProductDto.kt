f4d4294476a1a21be799e20e59ef8e43d7032b99
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-21 20:10:39 +0300

    незначительные изменения

--- app/src/main/java/ru/faserkraft/client/dto/ProductDto.kt
+++ app/src/main/java/ru/faserkraft/client/dto/ProductDto.kt
@@ -3,7 +3,7 @@ package ru.faserkraft.client.dto
 import com.google.gson.annotations.SerializedName
 
 data class ProductDto(
-    val id: Int,
+    val id: Long = 0,
     @SerializedName("serial_number")
     val serialNumber: String,
     @SerializedName("process_id")
@@ -11,4 +11,4 @@ data class ProductDto(
     @SerializedName("created_at")
     val createdAt: String,
     val steps: List<StepDto>
-)
+): ItemDto()
