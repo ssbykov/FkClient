090957abf082fe1b83bb0be7723cb049d1718fb9
Sergei Bykov <ss.bykoff@gmail.com>
2025-11-21 20:02:25 +0300

    добавил класс с ошибками

---
+++ app/src/main/java/ru/faserkraft/client/error/AppError.kt
@@ -0,0 +1,12 @@
+package ru.faserkraft.client.error
+
+import java.io.Serializable
+
+sealed class AppError(open var code: String) : RuntimeException(), java.io.Serializable
+
+class ApiError(val status: Int, override var code: String) : AppError(code), Serializable
+
+object NetworkError : AppError("error_network")
+object AwsError : AppError("aws_error")
+object UnknownError: AppError("error_unknown")
+object DaoError: AppError("dao_error")
