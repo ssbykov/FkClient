f9b5a3b53017d959bc47dec0946121d78e910f7a
Sergei Bykov <ss.bykoff@gmail.com>
2025-12-03 12:54:00 +0300

    убраны лишние импорты

--- app/src/main/java/ru/faserkraft/client/repository/ApiRepository.kt
+++ app/src/main/java/ru/faserkraft/client/repository/ApiRepository.kt
@@ -1,8 +1,8 @@
 package ru.faserkraft.client.repository
 
+import ru.faserkraft.client.dto.DeviceRequestDto
 import ru.faserkraft.client.dto.DeviceResponseDto
 import ru.faserkraft.client.dto.ProductDto
-import ru.faserkraft.client.dto.DeviceRequestDto
 
 interface ApiRepository {
     suspend fun getProduct(serialNumber: String): ProductDto
