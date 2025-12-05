37bd73ee5dbc75fc4394dc6647b5900fd4a1a526
Sergei Bykov <ss.bykoff@gmail.com>
2025-12-05 23:50:51 +0300

    добавлен метод закрытия этапа postStep

--- app/src/main/java/ru/faserkraft/client/repository/ApiRepositoryImpl.kt
+++ app/src/main/java/ru/faserkraft/client/repository/ApiRepositoryImpl.kt
@@ -4,6 +4,7 @@ import ru.faserkraft.client.api.Api
 import ru.faserkraft.client.dto.DeviceRequestDto
 import ru.faserkraft.client.dto.DeviceResponseDto
 import ru.faserkraft.client.dto.ProductDto
+import ru.faserkraft.client.dto.StepCloseDto
 import javax.inject.Inject
 
 class ApiRepositoryImpl @Inject constructor(
@@ -18,5 +19,9 @@ class ApiRepositoryImpl @Inject constructor(
 
     override suspend fun postDevice(device: DeviceRequestDto): DeviceResponseDto? {
         return baseRequest.post(api::postDevice, device)
+    }
+
+    override suspend fun postStep(step: StepCloseDto): ProductDto? {
+        return baseRequest.post(api::postStep, step)
     }
 }
