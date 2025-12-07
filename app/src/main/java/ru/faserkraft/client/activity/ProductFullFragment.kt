98685158c5e7678c4acd7e26cdd3dc92cc6150ad
Sergei Bykov <ss.bykoff@gmail.com>
2025-12-07 17:24:23 +0300

    добавил вниз кнопку возврата

--- app/src/main/java/ru/faserkraft/client/activity/ProductFullFragment.kt
+++ app/src/main/java/ru/faserkraft/client/activity/ProductFullFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.faserkraft.client.adapter.StepsAdapter
 import ru.faserkraft.client.databinding.FragmentProductFullBinding
@@ -44,5 +45,8 @@ class ProductFullFragment : Fragment() {
 
             adapter.submitList(product.steps)
         }
+
+        binding.btnBack.setOnClickListener { findNavController().navigateUp() }
+
     }
 }
