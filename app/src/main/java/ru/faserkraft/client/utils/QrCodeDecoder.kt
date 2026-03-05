9e0a04e29dced171d7f59387a6d611c04674dd60
Sergei Bykov <ss.bykoff@gmail.com>
2026-03-05 13:26:09 +0300

    добавил функцию isUfPkgCode

--- app/src/main/java/ru/faserkraft/client/utils/QrCodeDecoder.kt
+++ app/src/main/java/ru/faserkraft/client/utils/QrCodeDecoder.kt
@@ -28,3 +28,8 @@ fun isUfCode(str: String): Boolean {
     val pattern = Regex("^uf-\\d{9}$")
     return pattern.matches(str)
 }
+
+fun isUfPkgCode(str: String): Boolean {
+    val pattern = Regex("PKG-\\d{7}$")
+    return pattern.matches(str)
+}
