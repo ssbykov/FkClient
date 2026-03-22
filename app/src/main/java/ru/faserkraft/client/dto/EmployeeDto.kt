e7f55a57f5b9c934e900eeaa6e8a1bdedcea83a9
Sergei Bykov <ss.bykoff@gmail.com>
2026-03-22 19:02:42 +0300

    добавлен параметр user

--- app/src/main/java/ru/faserkraft/client/dto/EmployeeDto.kt
+++ app/src/main/java/ru/faserkraft/client/dto/EmployeeDto.kt
@@ -3,4 +3,11 @@ package ru.faserkraft.client.dto
 data class EmployeeDto(
     val id: Int,
     val name: String,
+    val user: UserDto
 )
+
+data class UserDto(
+    val id: Int,
+    val email: String,
+)
+
