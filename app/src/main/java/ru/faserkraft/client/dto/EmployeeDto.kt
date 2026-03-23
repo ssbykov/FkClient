09c9e5a800e8b0be08ada83fc2cdf81de706ab0c
Sergei Bykov <ss.bykoff@gmail.com>
2026-03-23 14:04:01 +0300

    добавил сериализацию

--- app/src/main/java/ru/faserkraft/client/dto/EmployeeDto.kt
+++ app/src/main/java/ru/faserkraft/client/dto/EmployeeDto.kt
@@ -1,10 +1,12 @@
 package ru.faserkraft.client.dto
 
+import java.io.Serializable
+
 data class EmployeeDto(
     val id: Int,
     val name: String,
     val user: UserDto
-)
+) : Serializable
 
 data class UserDto(
     val id: Int,
