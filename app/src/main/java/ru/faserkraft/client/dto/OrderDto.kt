b434ffeb18d79a126deafc86a76d84483af929f6
Sergei Bykov <ss.bykoff@gmail.com>
2026-04-13 14:50:16 +0300

    добавлены модели для работы с роутами заказов

---
+++ app/src/main/java/ru/faserkraft/client/dto/OrderDto.kt
@@ -0,0 +1,64 @@
+package ru.faserkraft.client.dto
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+// === Модели для чтения данных (ответов) ===
+
+data class OrderDto(
+    val id: Int = 0,
+    @SerializedName("contract_number")
+    val contractNumber: String,
+    @SerializedName("contract_date")
+    val contractDate: String,
+    @SerializedName("planned_shipment_date")
+    val plannedShipmentDate: String,
+    @SerializedName("shipment_date")
+    val shipmentDate: String?,
+    @SerializedName("shipment_by")
+    val shipmentBy: EmployeeDto?,
+    val items: List<OrderItemDto>,
+    val packaging: List<PackagingDto>
+) : ItemDto()
+
+data class OrderItemDto(
+    val id: Int = 0,
+    val quantity: Int,
+    @SerializedName("work_process")
+    val workProcess: ProcessDto
+) : ItemDto()
+
+
+// === Модели для отправки данных (запросов) ===
+
+data class OrderCreateDto(
+    @SerializedName("contract_number")
+    val contractNumber: String,
+    @SerializedName("contract_date")
+    val contractDate: String,
+    @SerializedName("planned_shipment_date")
+    val plannedShipmentDate: String
+)
+
+data class OrderUpdateDto(
+    val id: Int,
+    @SerializedName("contract_number")
+    val contractNumber: String,
+    @SerializedName("contract_date")
+    val contractDate: String,
+    @SerializedName("planned_shipment_date")
+    val plannedShipmentDate: String
+)
+
+data class OrderItemCreateDto(
+    @SerializedName("process_id")
+    val processId: Int,
+    val quantity: Int
+)
+
+data class OrderCloseDto(
+    @SerializedName("shipment_date")
+    val shipmentDate: String,
+    @SerializedName("shipment_by_id")
+    val shipmentById: Int
+)
