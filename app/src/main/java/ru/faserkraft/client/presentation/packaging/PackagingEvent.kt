a84f53ede89834330da32bc4474830ef5d04bbd9
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-03 12:50:08 +0300

    добавил NavigateToEdit, PackagingDeleted

--- app/src/main/java/ru/faserkraft/client/presentation/packaging/PackagingEvent.kt
+++ app/src/main/java/ru/faserkraft/client/presentation/packaging/PackagingEvent.kt
@@ -1,7 +1,9 @@
 package ru.faserkraft.client.presentation.packaging
 
 sealed interface PackagingEvent {
+    data class ShowError(val message: String) : PackagingEvent
     data object NavigateToPackaging : PackagingEvent
     data object NavigateToNewPackaging : PackagingEvent
-    data class ShowError(val message: String) : PackagingEvent
+    data object NavigateToEdit : PackagingEvent
+    data object PackagingDeleted : PackagingEvent
 }
