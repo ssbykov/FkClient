7da08ce124cce1b4078cfb97382f5a91548847bb
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-04 12:22:48 +0300

    восстановлена функция регистрации пользователя

--- app/src/main/java/ru/faserkraft/client/utils/QrCodeDecoder.kt
+++ app/src/main/java/ru/faserkraft/client/utils/QrCodeDecoder.kt
@@ -1,10 +1,5 @@
 package ru.faserkraft.client.utils
 
-import com.google.gson.Gson
-
-private val gson = Gson()
-
-
 fun isUfCode(str: String): Boolean {
     val pattern = Regex("^uf-\\d{7,9}$")
     return pattern.matches(str)
