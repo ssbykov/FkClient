72c9d4a1427fb0cf27825111926886eb490b6332
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-04 12:16:23 +0300

    восстановлена функция регистрации пользователя

--- app/src/main/java/ru/faserkraft/client/utils/QrCodeDecoder.kt
+++ app/src/main/java/ru/faserkraft/client/utils/QrCodeDecoder.kt
@@ -1,27 +1,9 @@
 package ru.faserkraft.client.utils
 
 import com.google.gson.Gson
-import com.google.gson.JsonParser
-import ru.faserkraft.client.dto.DeviceRegisterDto
-import ru.faserkraft.client.dto.DeviceRequestDto
-import ru.faserkraft.client.dto.deviceRegisterBuilder
 
 private val gson = Gson()
 
-fun qrCodeDecode(jsonString: String): Result<Any?> =
-    runCatching {
-        val obj = JsonParser.parseString(jsonString).getAsJsonObject()
-        val action = obj.get("action")?.asString
-
-        when (action) {
-            "register" -> {
-                val dataIn: DeviceRegisterDto =
-                    gson.fromJson(obj, DeviceRegisterDto::class.java)
-                deviceRegisterBuilder(dataIn)
-            }
-            else -> null
-        }
-    }
 
 fun isUfCode(str: String): Boolean {
     val pattern = Regex("^uf-\\d{7,9}$")
@@ -31,11 +13,4 @@ fun isUfCode(str: String): Boolean {
 fun isUfPkgCode(str: String): Boolean {
     val pattern = Regex("^PKG-\\d{7}$")
     return pattern.matches(str)
-}
-
-fun isDeviceRegistrationQr(str: String): Boolean {
-    return qrCodeDecode(str)
-        .getOrNull()
-        ?.let { it is DeviceRequestDto }
-        ?: false
 }
