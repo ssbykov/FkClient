abdff7cd7b510719867605fb3e94139a213bbec7
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-07 12:21:19 +0300

    добавил варианты событий в ProductEvent

--- app/src/main/java/ru/faserkraft/client/presentation/product/ProductEvent.kt
+++ app/src/main/java/ru/faserkraft/client/presentation/product/ProductEvent.kt
@@ -1,7 +1,23 @@
 package ru.faserkraft.client.presentation.product
 
+import ru.faserkraft.client.domain.model.Step
+
 sealed interface ProductEvent {
-    data object NavigateToProduct : ProductEvent
-    data object NavigateToNewProduct : ProductEvent
+    object NavigateToNewProduct : ProductEvent
+    object NavigateToProduct : ProductEvent
+    data class NavigateToEditProcess(val productId: Long) : ProductEvent
+    data class NavigateToEditStatus(val productId: Long) : ProductEvent
     data class ShowError(val message: String) : ProductEvent
+    data class ShowConfirmationDialog(
+        val title: String,
+        val message: String,
+        val actionType: ConfirmationActionType,
+        val step: Step? = null,
+    ) : ProductEvent
+}
+
+enum class ConfirmationActionType {
+    CHANGE_STATUS,
+    CHANGE_PROCESS,
+    CLOSE_STEP,
 }
