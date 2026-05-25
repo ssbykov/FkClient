e3a4f4d8642ee39cb76ffc7b1c2f93b43308fa89
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-25 12:58:03 +0300

    перенес в другой пакет

--- app/src/main/java/ru/faserkraft/client/presentation/inventory/InventoryContainerFragment.kt
+++ app/src/main/java/ru/faserkraft/client/presentation/inventory/InventoryContainerFragment.kt
@@ -1,4 +1,4 @@
-package ru.faserkraft.client.presentation.product.inventory
+package ru.faserkraft.client.presentation.inventory
 
 import android.app.AlertDialog
 import android.os.Bundle
--- app/src/main/java/ru/faserkraft/client/presentation/inventory/InventoryContainerPageAdapter.kt
+++ app/src/main/java/ru/faserkraft/client/presentation/inventory/InventoryContainerPageAdapter.kt
@@ -1,7 +1,9 @@
-package ru.faserkraft.client.presentation.product.inventory
+package ru.faserkraft.client.presentation.inventory
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import ru.faserkraft.client.presentation.inventory.overview.ProductsOverviewFragment
+import ru.faserkraft.client.presentation.inventory.overview.ProductsReworkScrapFragment
 
 
 class InventoryContainerPageAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
@@ -10,7 +12,7 @@ class InventoryContainerPageAdapter(fragment: Fragment) : FragmentStateAdapter(f
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> ProductsInventoryFragment()
+            0 -> ProductsOverviewFragment()
             1 -> ProductsReworkScrapFragment()
             else -> throw IllegalStateException("Invalid position: $position")
         }
