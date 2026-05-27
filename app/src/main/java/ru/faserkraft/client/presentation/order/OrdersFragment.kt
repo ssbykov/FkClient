39ef1987acfab1a8cfcc9e8035d72d54e02601be
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-27 11:36:52 +0300

    убрал логирование

--- app/src/main/java/ru/faserkraft/client/presentation/order/OrdersFragment.kt
+++ app/src/main/java/ru/faserkraft/client/presentation/order/OrdersFragment.kt
@@ -1,7 +1,6 @@
 package ru.faserkraft.client.presentation.order
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -140,7 +139,6 @@ class OrdersFragment : Fragment() {
         collectFlow(viewModel.events) { event ->
             when (event) {
                 is OrderEvent.ShowError -> {
-                    Log.e("OrdersFragment", "Order error: ${event.message}")
                     showErrorSnackbar(event.message)
                 }
 
