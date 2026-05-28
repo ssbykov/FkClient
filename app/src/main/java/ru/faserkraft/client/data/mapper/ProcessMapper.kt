dddeec35800927c208521c0076f1d7e983c96799
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-28 11:36:25 +0300

    добавил мапперы для ProcessShort, StepDefinitionWithProcess

--- app/src/main/java/ru/faserkraft/client/data/mapper/ProcessMapper.kt
+++ app/src/main/java/ru/faserkraft/client/data/mapper/ProcessMapper.kt
@@ -1,11 +1,15 @@
 package ru.faserkraft.client.data.mapper
 
-import ru.faserkraft.client.domain.model.FinishedProcess
-import ru.faserkraft.client.domain.model.Process
-import ru.faserkraft.client.domain.model.StepDefinition
 import ru.faserkraft.client.data.dto.FinishedProcessDto
 import ru.faserkraft.client.data.dto.ProcessDto
+import ru.faserkraft.client.data.dto.ProcessShortDto
 import ru.faserkraft.client.data.dto.StepDefinitionDto
+import ru.faserkraft.client.data.dto.StepDefinitionWithProcessDto
+import ru.faserkraft.client.domain.model.FinishedProcess
+import ru.faserkraft.client.domain.model.Process
+import ru.faserkraft.client.domain.model.ProcessShort
+import ru.faserkraft.client.domain.model.StepDefinition
+import ru.faserkraft.client.domain.model.StepDefinitionWithProcess
 
 fun StepDefinitionDto.toDomain(): StepDefinition = StepDefinition(
     id = id,
@@ -13,6 +17,7 @@ fun StepDefinitionDto.toDomain(): StepDefinition = StepDefinition(
     name = template.name,
     nameGenitive = template.nameGenitive,
 )
+
 
 fun ProcessDto.toDomain(): Process = Process(
     id = id,
@@ -28,3 +33,27 @@ fun FinishedProcessDto.toDomain(): FinishedProcess = FinishedProcess(
     sizeTypeName = type?.name,
     packagingCount = type?.packagingCount,
 )
+
+fun ProcessShortDto.toDomain(): ProcessShort {
+    return ProcessShort(
+        id = id,
+        name = name,
+    )
+}
+
+fun ProcessShort.toDto(): ProcessShortDto {
+    return ProcessShortDto(
+        id = id,
+        name = name,
+    )
+}
+
+fun StepDefinitionWithProcessDto.toDomain(): StepDefinitionWithProcess {
+    return StepDefinitionWithProcess(
+        id = id,
+        order = order,
+        name = template.name,
+        nameGenitive = template.nameGenitive,
+        process = process.toDomain(),
+    )
+}
