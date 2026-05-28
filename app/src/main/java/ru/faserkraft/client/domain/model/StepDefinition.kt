d5e54b78bf90bf566474d78a9e6fb5a7d5506b78
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-28 11:35:16 +0300

    добавил модель StepDefinitionWithProcessDto

--- app/src/main/java/ru/faserkraft/client/domain/model/StepDefinition.kt
+++ app/src/main/java/ru/faserkraft/client/domain/model/StepDefinition.kt
@@ -3,6 +3,14 @@ package ru.faserkraft.client.domain.model
 data class StepDefinition(
     val id: Int,
     val order: Int,
-    val name: String,      // из TemplateDto.name
-    val nameGenitive: String, // из TemplateDto.nameGenitive
+    val name: String,
+    val nameGenitive: String,
+)
+
+data class StepDefinitionWithProcess(
+    val id: Int,
+    val order: Int,
+    val name: String,
+    val nameGenitive: String,
+    val process: ProcessShort,
 )
