05b84ef458eedfe9862cbc816bc34d49deb3b9a3
Sergei Bykov <ss.bykoff@gmail.com>
2026-05-28 11:35:04 +0300

    добавил модель StepDefinitionWithProcessDto

--- app/src/main/java/ru/faserkraft/client/data/dto/StepDefinitionDto.kt
+++ app/src/main/java/ru/faserkraft/client/data/dto/StepDefinitionDto.kt
@@ -1,7 +1,18 @@
 package ru.faserkraft.client.data.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class StepDefinitionDto(
     val id: Int,
     val order: Int,
     val template: TemplateDto
+)
+
+data class StepDefinitionWithProcessDto(
+    val id: Int,
+    val order: Int,
+    @SerializedName("template")
+    val template: TemplateDto,
+    @SerializedName("work_process")
+    val process: ProcessShortDto,
 )
