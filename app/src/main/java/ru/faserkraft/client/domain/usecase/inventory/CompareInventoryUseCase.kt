fed6eac12cce59bef0c65efca25e9705af407547
Sergei Bykov <ss.bykoff@gmail.com>
2026-08-11 11:14:58 +0300

    изменил тип получаемого результата с бэкенда

--- app/src/main/java/ru/faserkraft/client/domain/usecase/inventory/CompareInventoryUseCase.kt
+++ app/src/main/java/ru/faserkraft/client/domain/usecase/inventory/CompareInventoryUseCase.kt
@@ -1,12 +1,12 @@
 package ru.faserkraft.client.domain.usecase.inventory
 
-import ru.faserkraft.client.domain.model.InventoryCompareResult
+import ru.faserkraft.client.domain.model.ProductInventoryCompareItem
 import ru.faserkraft.client.domain.repository.InventoryRepository
 import javax.inject.Inject
 
 class CompareInventoryUseCase @Inject constructor(
     private val repository: InventoryRepository,
 ) {
-    suspend operator fun invoke(inventoryId: Int): List<InventoryCompareResult> =
+    suspend operator fun invoke(inventoryId: Int): List<ProductInventoryCompareItem> =
         repository.compareInventory(inventoryId)
 }
