9e6e6c495b15948c41b56cc97254d7277a6fef1d
Sergei Bykov <ss.bykoff@gmail.com>
2026-08-21 11:55:09 +0300

    добавил обязательные специальные символы в пароль

--- app/src/main/java/ru/faserkraft/client/utils/PasswordGenerator.kt
+++ app/src/main/java/ru/faserkraft/client/utils/PasswordGenerator.kt
@@ -2,33 +2,30 @@ package ru.faserkraft.client.utils
 
 
 fun generatePassword(length: Int = 16): String {
+    require(length >= 4) {
+        "Password length must be at least 3"
+    }
+
     val upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     val lower = "abcdefghijklmnopqrstuvwxyz"
     val digits = "0123456789"
-    val special = "!@#\$%^&*()-_."
+    val special = $$"!@#$%^&*()-_."
     val allChars = upper + lower + digits + special
 
     val secureRandom = java.security.SecureRandom()
+    val chars = mutableListOf<Char>()
 
-    while (true) {
-        val chars = mutableListOf<Char>()
+    // Гарантируем обязательные классы
+    chars += upper[secureRandom.nextInt(upper.length)]
+    chars += lower[secureRandom.nextInt(lower.length)]
+    chars += digits[secureRandom.nextInt(digits.length)]
+    chars += special[secureRandom.nextInt(special.length)]
 
-        // Гарантируем обязательные классы
-        chars += upper[secureRandom.nextInt(upper.length)]
-        chars += lower[secureRandom.nextInt(lower.length)]
-        chars += digits[secureRandom.nextInt(digits.length)]
-
-        // Остальные символы — любые из разрешённых
-        repeat(length - chars.size) {
-            chars += allChars[secureRandom.nextInt(allChars.length)]
-        }
-
-        // Перемешиваем
-        chars.shuffle(secureRandom)
-
-        val password = chars.joinToString("")
-
-
-        return password
+    // Остальные символы — любые из разрешённых
+    repeat(length - chars.size) {
+        chars += allChars[secureRandom.nextInt(allChars.length)]
     }
+
+    chars.shuffle(secureRandom)
+    return chars.joinToString("")
 }
