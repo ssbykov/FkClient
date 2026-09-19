669ea6b76470b7e8a4a2e8ce1fcdc2e14f16da76
Sergei Bykov <ss.bykoff@gmail.com>
2026-09-19 08:45:10 +0300

    изменил название метода и добавил параметр по авансу

--- app/src/main/java/ru/faserkraft/client/domain/usecase/statistic/GetProductsStatisticsUseCase.kt
+++ app/src/main/java/ru/faserkraft/client/domain/usecase/statistic/GetProductsStatisticsUseCase.kt
@@ -4,13 +4,17 @@ import ru.faserkraft.client.domain.model.PeriodStatistics
 import ru.faserkraft.client.domain.repository.ProductRepository
 import javax.inject.Inject
 
-
 class GetProductsStatisticsUseCase @Inject constructor(
     private val repository: ProductRepository,
 ) {
     suspend operator fun invoke(
         dateFrom: String,
         dateTo: String,
+        includeFirstHalf: Boolean = false,
     ): PeriodStatistics =
-        repository.getFinishedProductsByPeriod(dateFrom, dateTo)
+        repository.getPeriodStatistics(
+            dateFrom = dateFrom,
+            dateTo = dateTo,
+            includeFirstHalf = includeFirstHalf,
+        )
 }
